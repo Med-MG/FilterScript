32f7ead8373ffebf7c74a0484d6826431c54a4c8
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-12-26 20:43:10 +0100

    Add project files.

---
+++ FilterScript/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilterScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
