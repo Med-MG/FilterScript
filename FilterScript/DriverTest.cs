3a2a20a021d4a3fab46584f746a6aee6dc81e58a
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-12-27 01:50:21 +0100

    Captcha Problem

--- FilterScript/DriverTest.cs
+++ FilterScript/DriverTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -27,10 +28,14 @@ namespace FilterScript
              * Close Page And Repeat Action.
              */
 
+            DateTime todayNow = DateTime.Now;
+
             //Driver Init
             IWebDriver driver = new ChromeDriver(@"C:\Users\Administrateur\Downloads\Production\YoucodeC#\FilterScript\ChromeDriver");
             //Email Input Locator
             var EmailInput = By.XPath("//*[@id='input-email-address']");
+            //Go button
+            var GoButton = By.XPath("/html/body/div[4]/div[1]/div[1]/form/div/div[2]/div/div[2]/button");
             //Excel Download Button
             var ExcelDownload = By.XPath("//*[@id='dt-grid1-buttons']/div/button[2]");
             //Launch Email Hippo website
@@ -46,29 +51,76 @@ namespace FilterScript
              * At the end Download Excel file
              */
             string fpath = @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\FilterScript\EmailLists\emails.txt";
+            string CheckedFile = @"C:\Users\Administrateur\Downloads\Production\YoucodeC#\FilterScript\EmailLists\Checkedemails.txt";
             if (File.Exists(fpath))
             {
-                FileInfo fi = new FileInfo(fpath);
-                //Open the file to read text
-                using(StreamReader sr = fi.OpenText()) 
-                {
-                    string txt;
-                    //Read the data from file, until the end of file is reached
-                    while((txt = sr.ReadLine()) != null)
-                    {
-                        //Check email for validity
-                        driver.FindElement(EmailInput).SendKeys(txt);
+                //FileInfo fi = new FileInfo(fpath);
+                //Put All lines in a list
 
-                        driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[1]/form/div/div[2]/div/div[2]/button")).Click();
-                        //wait for page to load agin then 
-                        WaitUntilElementVisible(EmailInput, driver, 30);
+                List<string> AllEmails = File.ReadAllLines(fpath).ToList();
+
+                FileInfo fiChecked = new FileInfo(CheckedFile);
+
+                for(int i = 0; i < 11; i++)
+                {
+                    //Check email for validity
+                    driver.FindElement(EmailInput).SendKeys(AllEmails[i]);
+                    driver.FindElement(GoButton).Click();
+
+                    //Put it in an Already checked file
+                    using (StreamWriter sw = fiChecked.CreateText()) 
+                    {
+                        sw.WriteLine(AllEmails[i]);
                     }
 
-                    WaitUntilElementVisible(ExcelDownload, driver, 30);
-                    //Download Excel File 
+                    AllEmails.RemoveAt(AllEmails.IndexOf(AllEmails[i]));
 
-
+                    
+                    //wait for page to load agin then 
+                    WaitUntilElementVisible(EmailInput, driver, 30);
                 }
+
+
+                //Open the file to read text
+                //using (StreamReader sr = fi.OpenText()) 
+                //{
+                //    string txt;
+                //    //Read the data from file, until the end of file is reached
+
+                //    for (int i = 0; i < 100; i++)
+                //    {
+                //        //Take email
+                //        txt = sr.ReadLine();
+                //        //Check email for validity
+                //        driver.FindElement(EmailInput).SendKeys(txt);
+                //        //Put it in an Already checked file
+                //        using (StreamWriter sw = fiChecked.CreateText())
+                //        {
+                //            sw.WriteLine(txt);
+                //        }
+
+                //    }
+
+                //while((txt = sr.ReadLine()) != null)
+                //{
+                //    //Check email for validity
+                //    driver.FindElement(EmailInput).SendKeys(txt);
+
+                //    driver.FindElement(By.XPath("/html/body/div[4]/div[1]/div[1]/form/div/div[2]/div/div[2]/button")).Click();
+                //    //wait for page to load agin then 
+                //    WaitUntilElementVisible(EmailInput, driver, 30);
+                //}
+
+
+                WaitUntilElementVisible(ExcelDownload, driver, 30);
+                //Download Excel File 
+                driver.FindElement(ExcelDownload).Click();
+
+
+                //Override the Old file with the new list
+                File.WriteAllLines(fpath, AllEmails.ToArray());
+
+                
             }
 
 
@@ -86,6 +138,7 @@ namespace FilterScript
                     throw;
                 }
             }
+
 
 
             //driver.Close();
